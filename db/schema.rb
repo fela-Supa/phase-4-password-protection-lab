199108725156bd7df12675830d92226760ca3776
fela-Supa <alvinaiterra@gmail.com>
2023-03-30 13:58:05 +0300

    password protection

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230330103638) do
+ActiveRecord::Schema.define(version: 2023_03_30_103638) do
 
-    create_table "users", force: :cascade do |t|
-        t.string "username"
-        t.string "password_digest"
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-    end
-    
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
